90acca8f02e4b5d46d18c55c483d8dc0372a15ef
Aditya Raj <aditya.rajb@gmail.com>
2021-11-11 12:15:15 +0530

    init

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "./css/variables.css";
+  import "./css/reset.css";
+</script>
+
+hi
+
+<style>
+  :global(body) {
+    background-color: var(--bg);
+    color: var(--fg);
+  }
+</style>
